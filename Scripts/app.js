37d8e3155b738bef956e87bf865c3682de4a9463
awen-xu <wxu59@my.centennialcollege.ca>
2018-05-23 12:16:32 -0400

    added list of favourite things

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,13 +2,15 @@
 
 // IIFE - Immediately Invoked Function Expression
 // anonymous self executing function
-(function () {
+let app = (function () {
     "use strict"
 
     // About Button Click event handler
+
     function AboutButtonClick() {
         console.log("About Button Clicked!");
     }
+
 
     function AboutButtonOver(event) {
         event.currentTarget.style.opacity = 0.3;
@@ -42,6 +44,103 @@
         let mySentence = "";
 
         paragraph.textContent = mySentence;
+
+        let myArray = [
+            {name: "Tom", age: 25},
+            {name: "Bob", age: 35},
+            {name: "Mike", age: 45},
+            {name: "Juan", age: 55},
+            {name: "Smyth", age: 65}
+        ];
+
+        myArray.push( {name: "Carol", age: 15});
+        myArray.unshift( {name: "Peter", age: 25});
+
+
+        let content = document.getElementsByClassName("content");
+        console.log("myArray length: " + myArray.length);
+
+        /* loop type # 1 - classic for loop
+        for(let index = 0; index < myArray.length; index++) {
+            console.log(myArray[index].name);
+        }
+        */
+
+        /* loop type # 2 - foreach (modern)
+        myArray.forEach(person => {
+            console.log(person.name);
+        });
+        */
+
+        /* loop type # 3 - foreach (classic)
+        myArray.forEach(function(person){
+            console.log(person.name);
+        });
+        */
+
+        /* loop type # 4 - for in (modern)
+        for (const index in myArray) {
+            console.log(myArray[index].name);
+        }
+        */
+
+        /* loop type # 5 - for of (modern)
+        for (const person of myArray) {
+            console.log(person.name);
+        }
+        */
+
+        /* loop type # 6 - while (classic)
+        let index = 0;
+        while (index < myArray.length) {
+            console.log(myArray[index].name);
+            index++;
+        }
+        */
+
+        let arrayEmpty;
+
+        // ternary operator - alternate toggle conditional statement
+        arrayEmpty = (myArray.length > 0 ) ?  false : true;
+
+        // === checks both value and type where == only checks value
+        if(myArray[0].age === 25) {
+            
+            console.log("First Element is Peter");
+        }
+
+
+        // associative arrays create this key / value pair association but there is no iterator
+        // which means you can't loop through them
+        let myAssociateArray = [];
+
+        myAssociateArray["Name"] = "Tom";
+        myAssociateArray["Age"] = 30;
+        myAssociateArray["StudentNum"] = "P008490";
+
+        console.log(myAssociateArray);
+        console.log(myAssociateArray["Name"]);
+
+        for (const element of myAssociateArray) {
+            console.log(element);
+        }
+
+        let myFavouriteThingsList = [
+            "Walking",
+            "Cubing",
+            "Animals",
+            "Games"
+        ];
+
+        // "hook into" a ul that is empty, that has an id of myFavouriteThings
+        let myFavouritesList = document.getElementById("myFavouriteThings");
+
+        for (let index = 0; index < myFavouriteThingsList.length; index++) {
+            let newItem = document.createElement("li");
+            newItem.textContent = myFavouriteThingsList[index];
+            myFavouritesList.appendChild(newItem);            
+        }
+
     }
 
     function Start() {
@@ -66,7 +165,15 @@
         }
     }
 
+
     //window.onload = Start;
 
     window.addEventListener("load", Start);
+
+    
+    return {
+        title: document.title
+
+    };
+    
 })();
