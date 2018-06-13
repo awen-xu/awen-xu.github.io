0149dbb77e365119f220483b398ea37ad945965a
awen-xu <wxu59@my.centennialcollege.ca>
2018-06-13 16:23:11 -0400

    fixed form patterns

--- Assignments/Assignment02/Scripts/contact.js
+++ Assignments/Assignment02/Scripts/contact.js
@@ -8,27 +8,15 @@
     let Message = document.getElementById("Message");
 
     function OutputFormDataToConsole() {
-
         console.log(`%c ---------------------------------------`, "color: blue;");
         console.log(`%c Form Data`, "font-weight:bold; font-size: 16px; color: blue;");
         console.log(`%c ---------------------------------------`, "color: blue;");
         console.log(`%c Full Name     : ${FirstName.value} ${LastName.value}`, "color: blue;");
         console.log(`%c Contact Number: ${ContactNumber.value}`, "color: blue;");
         console.log(`%c Email Address : ${EmailAddress.value}`, "color: blue;");
+        console.log(`%c Subject       : ${Subject.value}`, "color: blue;");
         console.log(`%c Your Message  : ${Message.value}`, "color: blue;");
         console.log(`%c ---------------------------------------`, "color: blue;");
-
-        console.log(`%c ---------------------------------------`, "color: blue;");
-        console.log(`%c Form Properties`, "font-weight:bold; font-size: 16px; color: blue;");
-        console.log(`%c ---------------------------------------`, "color: blue;");
-        console.log(`%c Form Length     : ${document.forms[0].length}`, "color: blue;");
-
-        for (let index = 0; index < document.forms[0].length; index++) {
-            console.log(`%c Form Element ${index}: ${document.forms[0].elements[index].value}`, "color: blue;");
-
-        }
-        // document.forms[0].submit();
-
     }
 
     function ClearValidationMessage() {
@@ -52,16 +40,16 @@
                 element.addEventListener("invalid", function () {
                     switch (element.id) {
                         case "FirstName":
-                            element.setCustomValidity("You must enter a name with at least 2 letters");
+                            element.setCustomValidity("Enter first name with at least 2 letters");
                             break;
                         case "LastName":
-                            element.setCustomValidity("You must enter a name with at least 2 letters");
+                            element.setCustomValidity("Enter last name with at least 2 letters");
                             break;
                         case "ContactNumber":
-                            element.setCustomValidity("You must enter a phone number with the pattern (###) ###-####");
+                            element.setCustomValidity("Enter a phone number with the pattern (###) ###-####");
                             break;
                         case "EmailAddress":
-                            element.setCustomValidity("You must enter an appropriate email address with the pattern name@example.com.");
+                            element.setCustomValidity("Enter an appropriate email address with the pattern name@example.com.");
                             break;
                         case "Subject":
                             element.setCustomValidity("You must enter a subject");
@@ -83,33 +71,29 @@
     }
 
     function ContactContent() {
-        //inserts text
+        //inserts text - title and instructions for form
         let title = "Send Me A Message";
         let contactHeader = document.getElementById("contactTitle");
         contactHeader.textContent = title;
-
         let myContactStatement = "Write a message with the form below, and I will get back to you as soon as possible!";
         let contactInfo = document.getElementById("contactInfo");
         contactInfo.textContent = myContactStatement;
-
         let myContactInstructions = "* Required";
         let contactInstruction = document.getElementById("contactInstructions");
         contactInstruction.textContent = myContactInstructions;
 
         ClearValidationMessage();
-        //document.forms[0].autocomplete = true;
 
         // create a new HTML Element
         let cancelButton = document.createElement("button");
         // configure the HTML Element
-        cancelButton.setAttribute("class", "btn btn-warning");
+        cancelButton.setAttribute("class", "btn btn-secondary btn-block");
         cancelButton.classList.add("btn-lg");
         cancelButton.textContent = "Cancel";
         cancelButton.addEventListener("click", function (event) {
             event.preventDefault();
             window.open("a01_index.html", "_parent");
         });
-
         // add the HTML Element to the page somewhere 
         // in this case I'm attaching a button to the first forml element
         document.forms[0].appendChild(cancelButton);
@@ -117,12 +101,10 @@
 
         let SendButton = document.getElementById("submitButton");
         SendButton.addEventListener("click", (event) => {
-            // event.preventDefault();
             if (!document.forms[0].checkValidity()) {
                 OutputFormDataToConsole();
                 ValidateForm();
             }
-            console.log(`Form Validates: ${document.forms[0].checkValidity()}`);
         });
     }
 
